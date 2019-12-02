9c90645088a149a769d7fb0eaa32177a9ae190a1
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-12-01 21:25:31 -0500

    Updates

---
+++ precache-manifest.d30847e629c6d6c7ed7cbb200b352aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71fa3c990b4cfe1b5365731fe52a67fe",
+    "url": "/petland/index.html"
+  },
+  {
+    "revision": "d0dfdbd3e138ed29d89a",
+    "url": "/petland/static/css/main.7ca2b7c3.chunk.css"
+  },
+  {
+    "revision": "f214096366dc26a635ae",
+    "url": "/petland/static/js/2.e0e8e9bd.chunk.js"
+  },
+  {
+    "revision": "d0dfdbd3e138ed29d89a",
+    "url": "/petland/static/js/main.c5e6135b.chunk.js"
+  },
+  {
+    "revision": "ffa103fce506ab6882ad",
+    "url": "/petland/static/js/runtime-main.1fb0ceab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/petland/static/media/logo.25bf045c.svg"
+  }
+]);
